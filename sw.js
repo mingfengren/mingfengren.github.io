430910abb06d12ff893822705d52f2bbc2802d2e
mingfengren <relifeme222@outlook.com>
2024-11-18 21:03:10 +0800

    Site updated: 2024-11-18 21:03:09

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731934356712";
+const VERSION = "1731934779501";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
